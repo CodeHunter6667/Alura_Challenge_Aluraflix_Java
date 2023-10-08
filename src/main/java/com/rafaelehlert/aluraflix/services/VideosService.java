4290bb736d87c36b2d5372f6b29e4bd8dd578150
Rafael Ehlert <ra.faehlert@hotmail.com>
2023-10-08 17:06:39 -0300

    classe VideosService metodos da arquitetura CRUD

--- src/main/java/com/rafaelehlert/aluraflix/services/VideosService.java
+++ src/main/java/com/rafaelehlert/aluraflix/services/VideosService.java
@@ -1,6 +1,7 @@
 package com.rafaelehlert.aluraflix.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -9,7 +10,10 @@ import org.springframework.transaction.annotation.Transactional;
 import com.rafaelehlert.aluraflix.dto.VideosDTO;
 import com.rafaelehlert.aluraflix.models.Videos;
 import com.rafaelehlert.aluraflix.repositories.VideosRepository;
+import com.rafaelehlert.aluraflix.services.exceptions.DatabaseException;
 import com.rafaelehlert.aluraflix.services.exceptions.ResourceNotFoundException;
+
+import jakarta.persistence.EntityNotFoundException;
 
 
 @Service
@@ -28,14 +32,38 @@ public class VideosService {
     @Transactional(readOnly = true)
     public Page<VideosDTO> findAll(Pageable pageable) {
         Page<Videos> result = repository.findAll(pageable);
-        return result.map(x -> new VideosDTO(x.getId(), x.getTitulo(), x.getDescricao(),x.getUrl()));
+        return result.map(x -> new VideosDTO(x.getId(), x.getTitulo(), x.getDescricao(), x.getUrl()));
     }
 
+    @Transactional
     public VideosDTO insert(VideosDTO dto) {
         Videos entity = new Videos();
         copyDtoToEntity(dto, entity);
         entity = repository.save(entity);
         return new VideosDTO(entity.getId(), entity.getTitulo(), entity.getDescricao(), entity.getUrl());
+    }
+
+    @Transactional
+    public VideosDTO update(Long id, VideosDTO dto) {
+        try {
+            Videos entity = repository.getReferenceById(id);
+            copyDtoToEntity(dto, entity);
+            entity = repository.save(entity);
+            return new VideosDTO(entity.getId(), entity.getTitulo(), entity.getDescricao(), entity.getUrl());
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException("Recurso não encontrado");
+        }
+    }
+
+    public void delete(Long id) {
+        if (!repository.existsById(id)) {
+            throw new ResourceNotFoundException("Recurso não encontrado");
+        }
+        try {
+            repository.deleteById(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException("Falha de integridade relacional");
+        }
     }
 
     private void copyDtoToEntity(VideosDTO dto, Videos entity) {
