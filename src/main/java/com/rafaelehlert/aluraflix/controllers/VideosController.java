b309e55b5011ee0e82c3780870dcd59828d14563
Rafael Ehlert <ra.faehlert@hotmail.com>
2024-05-11 13:31:46 -0300

    feat: criado restricões de requisições em Videos

--- src/main/java/com/rafaelehlert/aluraflix/controllers/VideosController.java
+++ src/main/java/com/rafaelehlert/aluraflix/controllers/VideosController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -40,6 +41,7 @@ public class VideosController {
     }
 
     @PostMapping
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN')")
     public ResponseEntity<VideosDTO> insert(@Valid @RequestBody VideosDTO dto) {
         dto = service.insert(dto);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
@@ -48,12 +50,14 @@ public class VideosController {
     }
 
     @PutMapping(value = "/{id}")
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN')")
     public ResponseEntity<VideosDTO> update(@PathVariable Long id, @Valid @RequestBody VideosDTO dto) {
         dto = service.update(id, dto);
         return ResponseEntity.ok(dto);
     }
     
     @DeleteMapping(value = "/{id}")
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN')")
     public ResponseEntity<Void> delete(@PathVariable Long id) {
         service.delete(id);
         return ResponseEntity.noContent().build();
