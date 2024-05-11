600f33b8297401d8dd7ebde0da2e40aabe66ec38
Rafael Ehlert <ra.faehlert@hotmail.com>
2024-05-11 12:29:27 -0300

    feat: serviço de geração de token

---
+++ src/main/java/com/rafaelehlert/aluraflix/security/TokenService.java
@@ -0,0 +1,51 @@
+package com.rafaelehlert.aluraflix.security;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.rafaelehlert.aluraflix.models.Usuario;
+
+@Service
+public class TokenService {
+	
+	@Value("${api.security.token.secret}")
+    private String secret;
+
+    public String gerarToken(Usuario usuario){
+        try{
+            Algorithm algoritmo = Algorithm.HMAC256(secret);
+            return JWT.create()
+                    .withIssuer("API_Controle_de_gastos")
+                    .withSubject(usuario.getUsername())
+                    .withExpiresAt(dataEspiracao())
+                    .sign(algoritmo);
+        }catch (JWTCreationException e){
+            throw new RuntimeException("Erro ao gerar o token JWT ", e);
+        }
+    }
+
+    public String getSubject(String tokenJwt){
+        try{
+            Algorithm algoritmo = Algorithm.HMAC256(secret);
+            return JWT.require(algoritmo)
+                    .withIssuer("API_Controle_de_gastos")
+                    .build()
+                    .verify(tokenJwt)
+                    .getSubject();
+        } catch (JWTVerificationException e){
+            return null;
+        }
+    }
+
+    private Instant dataEspiracao(){
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+    }
+}
