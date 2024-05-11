8fadee7c78d625d8076780af48442f53ee13a3b1
Rafael Ehlert <ra.faehlert@hotmail.com>
2024-05-11 12:29:02 -0300

    feat: filtro de autenticação

---
+++ src/main/java/com/rafaelehlert/aluraflix/security/SecurityFilter.java
@@ -0,0 +1,47 @@
+package com.rafaelehlert.aluraflix.security;
+
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.rafaelehlert.aluraflix.repositories.UsuarioRepository;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter{
+
+	@Autowired
+    private TokenService service;
+
+    @Autowired
+    private UsuarioRepository repository;
+    
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        var tokenJwt = recuperaToken(request);
+        var login = service.getSubject(tokenJwt);
+        
+        if (login != null){
+            var usuario = repository.procuraUserPorUsername(login);
+            var authentication = new UsernamePasswordAuthenticationToken(usuario, null, usuario.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private String recuperaToken(HttpServletRequest request){
+        var authorizationHeader = request.getHeader("Authorization");
+        if (authorizationHeader != null){
+            return authorizationHeader.replace("Bearer ", "");
+        }
+        return null;
+    }
+}
