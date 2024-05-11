49915fad3cdabc5124a448288955e94bd2dc59c8
Rafael Ehlert <ra.faehlert@hotmail.com>
2024-05-11 13:29:57 -0300

    fix: alterando metodo de busca de Role

--- src/main/java/com/rafaelehlert/aluraflix/repositories/RolesRepository.java
+++ src/main/java/com/rafaelehlert/aluraflix/repositories/RolesRepository.java
@@ -2,8 +2,10 @@ package com.rafaelehlert.aluraflix.repositories;
 
 import com.rafaelehlert.aluraflix.models.Roles;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface RolesRepository extends JpaRepository<Roles, Long> {
 
-    Roles findByRolesName(String roleName);
+    @Query("SELECT obj FROM Roles obj WHERE obj.nomeRole = :roleName")
+    Roles searchByNomeRole(String roleName);
 }
