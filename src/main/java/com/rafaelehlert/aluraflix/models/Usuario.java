200c5cc8585a818dd46bb4732c67534b5cafd8ad
Rafael Ehlert <ra.faehlert@hotmail.com>
2024-05-11 12:26:30 -0300

    feat: criada entidade Usuario

---
+++ src/main/java/com/rafaelehlert/aluraflix/models/Usuario.java
@@ -0,0 +1,109 @@
+package com.rafaelehlert.aluraflix.models;
+
+import java.util.*;
+
+import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@Entity
+@Table(name = "tb_usuario")
+public class Usuario implements UserDetails{
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String username;
+	private String password;
+    @ManyToOne
+	@JoinColumn(name = "role_id")
+    private Roles role;
+	
+	public Usuario() {
+		
+	}
+
+	public Usuario(Long id, String username, String password, Roles roles) {
+		this.id = id;
+		this.username = username;
+		this.password = password;
+		this.role = roles;	
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Roles getRoles() {
+		return role;
+	}
+
+	public void setRoles(Roles roles) {
+		this.role = roles;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Usuario other = (Usuario) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return Collections.singletonList(role);
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+}
