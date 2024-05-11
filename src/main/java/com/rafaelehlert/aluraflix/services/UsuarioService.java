7e088ccddf92d78524036c7c7d25d95ed3f0aa37
Rafael Ehlert <ra.faehlert@hotmail.com>
2024-05-11 12:31:44 -0300

    feat: criado Service de Usuario

---
+++ src/main/java/com/rafaelehlert/aluraflix/services/UsuarioService.java
@@ -0,0 +1,41 @@
+package com.rafaelehlert.aluraflix.services;
+
+import com.rafaelehlert.aluraflix.models.Roles;
+import com.rafaelehlert.aluraflix.repositories.RolesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.rafaelehlert.aluraflix.dto.UsuarioDTO;
+import com.rafaelehlert.aluraflix.models.Usuario;
+import com.rafaelehlert.aluraflix.repositories.UsuarioRepository;
+
+import jakarta.transaction.Transactional;
+
+@Service
+public class UsuarioService {
+
+	@Autowired
+	private UsuarioRepository repository;
+
+	@Autowired
+	private RolesRepository rolesRepository;
+	
+	@Transactional
+	public UsuarioDTO insert(UsuarioDTO dto) {
+		Roles role = rolesRepository.findByRolesName(dto.getRoles());
+		Usuario usuario = new Usuario();
+		copyDtoToEntity(dto, usuario);
+		usuario.setRoles(role);
+		var senhaCriptocrafada = new BCryptPasswordEncoder().encode(usuario.getPassword());
+		usuario.setPassword(senhaCriptocrafada);
+		repository.save(usuario);
+		return new UsuarioDTO(usuario);
+	}
+	
+	private static void copyDtoToEntity(UsuarioDTO dto, Usuario entity) {
+		entity.setId(dto.getId());
+		entity.setUsername(dto.getUsername());
+		entity.setPassword(dto.getPassword());
+	}
+}
